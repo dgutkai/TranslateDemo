6c3d2c9056ce95a782f52227cf8237220d0d2bfe
704299659@qq.com <704299659@qq.com>
2018-01-24 08:59:10 +0800

    【新建】创建项目

---
+++ TranslateDemo/TranslateDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TranslateDemo
+//
+//  Created by lanmi on 2018/1/24.
+//  Copyright © 2018年 com.qcymail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
